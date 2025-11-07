365fcf47a2261567d31109b33bac110e0a7f43d5
RajinderSingh <rajinder.singh@zooplus.com>
2025-11-07 16:41:47 +0100

    with documentaiton

--- cmd/query-service/main.go
+++ cmd/query-service/main.go
@@ -83,4 +83,4 @@ func (app *App) handleQuery(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(events)
-}
+	log.Printf("Returned %d events", len(events))}
